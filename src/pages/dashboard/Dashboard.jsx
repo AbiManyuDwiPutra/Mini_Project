2acd59880e756d5279117a6ddcc4e3a91bbfe9b8
afu <bs.sochu@gmail.com>
2023-05-08 19:36:26 +0700

    tab data list

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,4 +1,5 @@
 import {
+  Tabs,
   Button,
   Form,
   Input,
@@ -14,45 +15,48 @@ import React, { useEffect, useState } from "react";
 import Gap from "../../components/gap/gap";
 import { useQuery } from "@apollo/client";
 import { GET_OFFLINE_CLASS } from "../OfflineClass/query";
+import { GET_PROFILE } from "../loginPage/query/profile-query"
 import LoadingComponent from "../../components/loadingComponent/LoadingComponent";
 
 const FormComponentExp = () => {
   const { Title } = Typography;
 
-  const { data: consumenData, loading: isProductLoading } =
-    useQuery(GET_OFFLINE_CLASS);
+  const { data: dataProfile, loading: isLoadingDataProfile } =
+    useQuery(GET_PROFILE);
 
-  const [data = consumenData?.offline_class, setData] = useState();
+
+  const { data: dataOfflineClass, loading: isLoadingDataOC } =
+  useQuery(GET_OFFLINE_CLASS);
+
+
+  const [dataUser = dataProfile?.profile, setDataUser] = useState();
+  const [dataOC = dataOfflineClass?.offline_class, setDataOC] = useState();
 
   // const [data, setData] = useState(INITIAL_TABLE_DATA);
-
-  const TABLE_COLUMNS = [
+  const DATA_OC_COLUMN_OPT = [
     {
       title: "Nama",
       dataIndex: "nama",
-      key: "id",
     },
     {
       title: "Alamat",
       dataIndex: "alamat",
-      key: "id",
     },
     {
       title: "Nomor HP",
       dataIndex: "no_hp",
-      key: "id",
     },
     {
       title: "Action",
       dataIndex: "action",
       render: (_, record) =>
-        data.length >= 1 ? (
+        dataOC.length >= 1 ? (
           <Space>
             {/* <a onClick={() => handleEdit(record)}>Edit</a> */}
             <Popconfirm
               title="Sure to delete?"
               arrow={false}
-              onConfirm={() => deleteData(record.key)}
+              onConfirm={() => deleteDataOC(record.key)}
             >
               <a>Delete</a>
             </Popconfirm>
@@ -61,29 +65,79 @@ const FormComponentExp = () => {
     },
   ];
 
-  const deleteData = (key) => {
-    const newData = data.filter((item) => item.key !== key);
-    setData(newData);
+  const DATA_PROFILE_COLUMN_OPT = [
+    {
+      title: "Username",
+      dataIndex: "username",
+    },
+    {
+      title: "Password",
+      dataIndex: "password",
+    },
+    {
+      title: "Timestamp",
+      dataIndex: "timestamp",
+    },
+    {
+      title: "Action",
+      dataIndex: "action",
+      render: (_, record) =>
+        dataOC.length >= 1 ? (
+          <Space>
+            {/* <a onClick={() => handleEdit(record)}>Edit</a> */}
+            <Popconfirm
+              title="Sure to delete?"
+              arrow={false}
+              onConfirm={() => deleteDataUser(record.key)}
+            >
+              <a>Delete</a>
+            </Popconfirm>
+          </Space>
+        ) : null,
+    },
+  ];
+
+  const TabItem = [
+    {
+      key: '1',
+      label: `User List`,
+      children: <Table columns={DATA_PROFILE_COLUMN_OPT} dataSource={dataUser} rowKey={"username"} />,
+    },
+    {
+      key: '2',
+      label: `Offline Class List`,
+      children: <Table columns={DATA_OC_COLUMN_OPT} dataSource={dataOC} rowKey={"id"} />,
+    }
+  ];
+
+  const deleteDataOC = (key) => {
+    const newData = dataOC.filter((item) => item.key !== key);
+    setDataOC(newData);
   };
 
-  // useEffect(() => {}, []);
+  const deleteDataUser = (key) => {
+    const newData = dataUser.filter((item) => item.key !== key);
+    setDataUser(newData)
+  }
+
+  useEffect(() => {}, []);
 
   return (
     <>
-      {isProductLoading ? (
+      {/* {isProductLoading ? (
         <LoadingComponent />
       ) : (
-        <>
+        <> */}
           <Title>Form Biodata Mahasiswa</Title>
 
           <Gap height={30} />
 
           {/* Table */}
           {/* {data?.map((item) => ( */}
-            <Table columns={TABLE_COLUMNS} dataSource={data} />
+           <Tabs defaultActiveKey="1" items={TabItem} />;
           {/* ))} */}
-        </>
-      )}
+        {/* </> */}
+       {/* )} */}
     </>
   );
 };
